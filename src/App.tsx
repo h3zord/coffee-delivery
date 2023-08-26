acdc7d83114bca4f6a3f66c60e94e49a739e59f8
h3zord <lucaschmain@gmail.com>
2023-08-26 12:29:29 -0300

    build: building default themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
